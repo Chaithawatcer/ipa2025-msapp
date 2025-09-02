fc33ccac017d96045ae01190c179c0bf425862e2
Chaithawatcer <66070046@kmitl.ac.th>
2025-09-02 15:46:28 +0700

    part-12

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -1,12 +1,27 @@
+import os
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-channel = connection.channel()
 
-channel.exchange_declare(exchange="jobs", exchange_type="direct")
-channel.queue_declare(queue="router_jobs")
-channel.queue_bind(queue="router_jobs", exchange="jobs", routing_key="check_interfaces")
+def produce(host, body):
 
-channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body="192.168.1.44")
+    rabbitmq_user = os.getenv("RABBITMQ_DEFAULT_USER")
+    rabbitmq_pass = os.getenv("RABBITMQ_DEFAULT_PASS")
 
-connection.close()
+    credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_pass)
+    parameters = pika.ConnectionParameters(host, credentials=credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange="jobs", exchange_type="direct")
+    channel.queue_declare(queue="router_jobs")
+    channel.queue_bind(
+        queue="router_jobs", exchange="jobs", routing_key="check_interfaces"
+    )
+
+    channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
+
+    connection.close()
+
+
+if __name__ == "__main__":
+    produce("localhost", "192.168.1.44")
